498812bc9721a9db7ab9e06e9b4388a29f6c39ee
Aaron Ouellette <aaronjo2@illinois.edu>
2026-03-18 21:52:09 -0500

    more doc strings

--- src/integrators.rs
+++ src/integrators.rs
@@ -1,5 +1,10 @@
 use crate::SimState;
 
+/// Compute one simulation step using the symmetric Euler method.
+///
+/// $x^{n+1} = x^n + v^n \Delta t$
+/// $v^{n+1} = v^n + a^{n+1} \Delta t$
+/// This is a first-order, time-symmetric method.
 pub fn step_euler<const NDIM: usize>(sim_state: &mut SimState<NDIM>, dt: f64) {
     for i in 0..sim_state.particles.len() {
         for d in 0..NDIM {
@@ -14,6 +19,12 @@ pub fn step_euler<const NDIM: usize>(sim_state: &mut SimState<NDIM>, dt: f64) {
     }
 }
 
+/// Compute one simulation step using the leapfrog method.
+///
+/// 1/2-Kick: $v^{n+1/2} = v^n + a^n \Delta t/2$
+/// Drift: $x^{n+1} = x^n + v^{n+1/2} \Delta t$
+/// 1/2-Kick: $v^{n+1} = v^{n+1/2} + a^{n+1} \Delta t/2$
+/// This is a second-order, time-symmetric method.
 pub fn step_leapfrog<const NDIM: usize>(sim_state: &mut SimState<NDIM>, dt: f64) {
     for i in 0..sim_state.particles.len() {
         // kick
@@ -34,6 +45,7 @@ pub fn step_leapfrog<const NDIM: usize>(sim_state: &mut SimState<NDIM>, dt: f64)
     }
 }
 
+/// Time-integrate the system for given total time.
 pub fn integrate<const NDIM: usize>(
     sim_state: &mut SimState<NDIM>,
     total_time: f64,
